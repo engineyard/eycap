5d09fb4b3a4eb31812a5980cc81c151272450fbe
atmos <atmos@atmos.org>
2008-02-06 15:51:40 -0700

    add memcached fixups

--- lib/eycap/recipes/memcached.rb
+++ lib/eycap/recipes/memcached.rb
@@ -1,7 +1,5 @@
 Capistrano::Configuration.instance(:must_exist).load do
-
-  namespace :memcached do
-    
+  namespace :memcached do 
     desc "Start memcached"
     task :start, :roles => :app, :only => {:memcached => true} do
       sudo "/etc/init.d/memcached start"
@@ -14,6 +12,9 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :restart, :roles => :app, :only => {:memcached => true} do
       sudo "/etc/init.d/memcached restart"
     end        
+    desc "Symlink the memcached.yml file into place if it exists"
+    task :symlink_configs, :roles => :app, :only => {:memcached => true } do
+      run "if [ -f #{shared_path}/config/memcached.yml ]; then ln -nfs #{shared_path}/config/memcached.yml #{release_path}/config/memcached.yml; fi"
+    end
   end
-
 end
--- lib/eycap/version.rb
+++ lib/eycap/version.rb
@@ -2,7 +2,7 @@ module Eycap #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 2
-    TINY  = 2
+    TINY  = 4
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
