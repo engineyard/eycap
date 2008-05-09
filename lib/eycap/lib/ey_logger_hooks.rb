38a637efb653d49a4e20193101dc7263d41753b1
Daniel Neighman <has.sox@gmail.com>
2008-05-09 14:57:34 +1000

    Updates the loggers post process hook to create the deploy_logs directory.  Must've sliped in the change over

--- lib/eycap/lib/ey_logger_hooks.rb
+++ lib/eycap/lib/ey_logger_hooks.rb
@@ -5,6 +5,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :ey_logger do
     task :upload_log_to_slice, :except => { :no_release => true} do
       logger = Capistrano::EYLogger
+      run "mkdir -p #{shared_path}/deploy_logs"
       put File.open(logger.log_file_path).read, "#{shared_path}/deploy_logs/#{logger.remote_log_file_name}"
     end
   end
