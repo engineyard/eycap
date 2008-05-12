ab900af2c3fcf3b06dba98adffb884c07a1042c5
Tim Carey-Smith <tim@spork.in>
2008-05-12 14:52:55 +1200

    Add juggernaut recipes

---
+++ lib/eycap/recipes/juggernaut.rb
@@ -0,0 +1,18 @@
+Capistrano::Configuration.instance(:must_exist).load do
+
+  namespace :juggernaut do
+    desc "After update_code you want to symlink the juggernaut.yml file into place"
+    task :symlink_configs, :roles => :app, :except => {:no_release => true} do
+      run <<-CMD
+        cd #{release_path} &&
+        ln -nfs #{shared_path}/config/juggernaut.yml #{release_path}/config/juggernaut.yml
+      CMD
+    end
+    [:start,:stop,:restart].each do |op|
+      desc "#{op} juggernaut server"
+      task op, :roles => :app, :except => {:no_release => true} do
+        sudo "/usr/bin/monit #{op} all -g juggernaut_#{application}"
+      end
+    end
+  end
+end
