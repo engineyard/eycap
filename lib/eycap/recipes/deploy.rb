12172e4fc028f0fae265a7bbf656684bb2d86add
Dylan Egan <me@dylanegan.com>
2008-06-17 16:55:11 +1000

    Add merb migration support from matta [https://github.com/matta]. github wasnt working to pull in change from ezmobius fork.

--- lib/eycap/recipes/deploy.rb
+++ lib/eycap/recipes/deploy.rb
@@ -18,6 +18,44 @@ Capistrano::Configuration.instance(:must_exist).load do
         ln -nfs #{shared_path}/config/mongrel_cluster.yml #{latest_release}/config/mongrel_cluster.yml
       CMD
     end
+
+    desc <<-DESC
+      Run the migrate rake task. By default, it runs this in most recently \
+      deployed version of the app. However, you can specify a different release \
+      via the migrate_target variable, which must be one of :latest (for the \
+      default behavior), or :current (for the release indicated by the \
+      `current' symlink). Strings will work for those values instead of symbols, \
+      too. You can also specify additional environment variables to pass to rake \
+      via the migrate_env variable. Finally, you can specify the full path to the \
+      rake executable by setting the rake variable. The defaults are:
+ 
+      set :rake, "rake"
+      set :framework, "merb"
+      set :merb_env, "production"
+      set :migrate_env, ""
+      set :migrate_target, :latest
+    DESC
+    task :migrate, :roles => :db, :only => { :primary => true } do
+      rake = fetch(:rake, "rake")
+      
+      framework = fetch(:framework, "rails")
+      if framework.match(/^rails$/i)
+        app_env = fetch(:rails_env, "production")
+      else
+        app_env = fetch("#{framework.downcase}_env".to_sym, "production")
+      end
+      
+      migrate_env = fetch(:migrate_env, "")
+      migrate_target = fetch(:migrate_target, :latest)
+ 
+      directory = case migrate_target.to_sym
+        when :current then current_path
+        when :latest then current_release
+        else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
+        end
+ 
+      run "cd #{directory}; #{rake} #{framework.upcase}_ENV=#{app_env} #{migrate_env} db:migrate"
+    end
   
     desc "Display the maintenance.html page while deploying with migrations. Then it restarts and enables the site again."
     task :long do
