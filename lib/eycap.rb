5427fe63b2667819ba63e25459bc85c9c22a2236
Tyler Bird <tyler.bird@gmail.com>
2009-01-10 10:23:44 -0700

    updated passenger:restart task

--- lib/eycap.rb
+++ lib/eycap.rb
@@ -1,3 +1,3 @@
 module Eycap
-  VERSION = '0.4.0'
+  VERSION = '0.4.1'
 end
--- lib/eycap/recipes/passenger.rb
+++ lib/eycap/recipes/passenger.rb
@@ -3,7 +3,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc <<-DESC
     Restart the passenger module to reload the application after deploying.
     DESC
-    task :restart, :roles => [:app], :except => {:no_release => true} do
+    task :restart, :roles => :app, :except => {:no_release => true} do
       sudo "touch #{current_path}/tmp/restart.txt"
     end
   end
