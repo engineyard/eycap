35c69f12aa78b5ee6f99bd9c1b22980698076b1f
Shai Rosenfeld <shaiguitar@gmail.com>
2009-02-23 11:30:40 +0200

    adding a flush task for memcached

--- lib/eycap/recipes/memcached.rb
+++ lib/eycap/recipes/memcached.rb
@@ -12,6 +12,10 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :restart, :roles => [:app], :only => {:memcached => true} do
       sudo "/etc/init.d/memcached restart"
     end        
+    desc "Flush memcached - this assumes memcached is on port 11211"
+    task :flush, :roles => [:app], :only => {:memcached => true} do
+      sudo "echo 'flush_all' | nc localhost 11211"
+    end        
     desc "Symlink the memcached.yml file into place if it exists"
     task :symlink_configs, :roles => [:app], :only => {:memcached => true }, :except => { :no_release => true } do
       run "if [ -f #{shared_path}/config/memcached.yml ]; then ln -nfs #{shared_path}/config/memcached.yml #{latest_release}/config/memcached.yml; fi"
