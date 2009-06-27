2902fa0ddabfb5580663071dfc0d212a7a3b4741
Tyler Bird <tyler.bird@gmail.com>
2009-06-26 21:40:04 -0600

    removed condition for dbhost

--- lib/eycap.rb
+++ lib/eycap.rb
@@ -1,3 +1,3 @@
 module Eycap
-  VERSION = '0.4.11'
+  VERSION = '0.4.12'
 end
--- lib/eycap/recipes/database.rb
+++ lib/eycap/recipes/database.rb
@@ -21,7 +21,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
       if @environment_info['adapter'] == 'mysql'
         dbhost = @environment_info['host']
-        dbhost = environment_dbhost.sub('-master', '') + '-replica' if dbhost != 'localhost' # added for Solo offering, which uses localhost
+        dbhost = environment_dbhost.sub('-master', '') + '-replica'
         run "mysqldump --add-drop-table -u #{dbuser} -h #{dbhost} -p #{environment_database} | bzip2 -c > #{backup_file}.bz2" do |ch, stream, out |
            ch.send_data "#{dbpass}\n" if out=~ /^Enter password:/
         end
