984a4042931f4c9b24119d02c3b92146ed4a48f1
Corey Donohoe <atmos@atmos.org>
2009-07-30 17:35:55 -0600

    it helps if i look for the Gemfile via the absolute path :)

--- lib/eycap/recipes/bundler.rb
+++ lib/eycap/recipes/bundler.rb
@@ -3,9 +3,9 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Automatically installed your bundled gems if a Gemfile exists"
     task :bundle_gems do
       %w(vendor bin).each do |dirname|
-        run "if [ -f Gemfile ]; then mkdir -p #{shared_path}/bundling/#{dirname} && cd #{release_path} && ln -s #{shared_path}/bundling/#{dirname} .; fi"
+        run "if [ -f #{release_path}/Gemfile ]; then mkdir -p #{shared_path}/bundling/#{dirname} && cd #{release_path} && ln -s #{shared_path}/bundling/#{dirname} .; fi"
       end
-      run "cd #{release_path} && if [ -f Gemfile ]; then gem bundle; fi"
+      run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && gem bundle; fi"
     end
     after "deploy:symlink_configs","bundler:bundle_gems"
   end
