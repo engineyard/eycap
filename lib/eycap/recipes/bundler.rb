4795f33f7720d02a0f146ddb155f370604c695c9
Corey Donohoe <atmos@atmos.org>
2009-10-14 14:49:06 -0700

    updating to a more opinionated bundling strategy. applications should use the bundler's cache instead of symlinking on each deploy.  should make rolling back a lot simpler

--- lib/eycap/recipes/bundler.rb
+++ lib/eycap/recipes/bundler.rb
@@ -2,10 +2,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :bundler do
     desc "Automatically installed your bundled gems if a Gemfile exists"
     task :bundle_gems do
-      %w(vendor bin).each do |dirname|
-        run "if [ -f #{release_path}/Gemfile ]; then mkdir -p #{shared_path}/bundling/#{dirname} && cd #{release_path} && ln -s #{shared_path}/bundling/#{dirname} .; fi"
-      end
-      run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && gem bundle; fi"
+      run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && gem bundle --cached; fi"
     end
     after "deploy:symlink_configs","bundler:bundle_gems"
   end
