fa35db1b6d2383560d8b31a0977f08b9fc4dc7aa
Tyler Bird <tbird@jupiter.(none)>
2009-12-17 16:34:31 -0700

    renamed task cap slice:tail_production_logs to cap slice:tail_environment_logs

--- lib/eycap/recipes/slice.rb
+++ lib/eycap/recipes/slice.rb
@@ -1,15 +1,15 @@
 Capistrano::Configuration.instance(:must_exist).load do
 
   namespace :slice do
-    desc "Tail the Rails production log for this environment"
-    task :tail_production_logs, :roles => :app do
+    desc "Tail the Rails logs for your environment"
+    task :tail_environment_logs, :roles => :app do
       run "tail -f #{shared_path}/log/#{rails_env}.log" do |channel, stream, data|
         puts  # for an extra line break before the host name
         puts "#{channel[:server]} -> #{data}" 
         break if stream == :err    
       end
     end
-    desc "Tail the Mongrel logs this environment"
+    desc "Tail the Mongrel logs for your environment"
     task :tail_mongrel_logs, :roles => :app do
       run "tail -f #{shared_path}/log/mongrel*.log" do |channel, stream, data|
         puts  # for an extra line break before the host name
