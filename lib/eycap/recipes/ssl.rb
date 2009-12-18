6eea1d9df46c2f7f1c82bcf3fcde07522d29dd52
mriley <mriley@mrileys-macbook-pro.local>
2009-12-18 09:20:11 -0500

    Update to SSL recipe.

--- lib/eycap/recipes/ssl.rb
+++ lib/eycap/recipes/ssl.rb
@@ -5,16 +5,15 @@ Capistrano::Configuration.instance(:must_exist).load do
       desc "create 1024 bit csr and key for ssl certificates"
       task :short, :roles => :app, :except => {:no_release => true} do
         sudo "mkdir -p /data/ssl/"
-        run "openssl req -new -days 365 -newkey rsa:1024 -keyout #{application}.com.key -out #{application}.com.csr"
-        run "openssl rsa -in #{application}.com.key -out #{application}.com.key"
+        run "cd /data/ssl/"
+        run "openssl req -new -nodes -days 365 -newkey rsa:1024 -subj '/C=US/ST=Oregon/L=Portland/CN=www.madboa.com' -keyout #{application}.com.key -out #{application}.com.csr"
       end
       
       desc "create 2048 bit csr and key for ssl certificates"
       task :long, :roles => :app, :except => {:no_release => true} do
         sudo "mkdir -p /data/ssl/"
-        run "openssl req -new -days 365 -newkey rsa:2048 -keyout #{application}.com.key -out #{application}.com.csr"
-        run "openssl rsa -in #{application}.com.key -out #{application}.com.key"
+        run "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout #{application}.com.key -out #{application}.com.csr"
       end
-    end
+    end    
   end
 end
