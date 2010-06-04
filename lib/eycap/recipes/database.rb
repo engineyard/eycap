e8d1a93cc752e830e6f232bdd2f436fec461245f
mriley <mriley@mrileys-macbook-pro.local>
2010-06-04 17:01:59 -0400

    updated database.rb

--- lib/eycap/recipes/database.rb
+++ lib/eycap/recipes/database.rb
@@ -12,45 +12,39 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :clone_prod_to_staging, :roles => :db, :only => { :primary => true } do
 
       # This task currently runs only on traditional EY offerings.
-      # You need to have both a production and staging environment defined in 
-      # your deploy.rb file.  
+      # You need to have both a production and staging environment defined in
+      # your deploy.rb file.
 
-      backup_name
-      on_rollback { run "rm -f #{backup_file}" }
-      run("cat #{shared_path}/config/database.yml") { |channel, stream, data| @environment_info = YAML.load(data)[rails_env] }  
+      backup_name unless exists?(:backup_file)
+      run("cat #{shared_path}/config/database.yml") { |channel, stream, data| @environment_info = YAML.load(data)[rails_env] }
+      dump
 
       if @environment_info['adapter'] == 'mysql'
-        dbhost = @environment_info['host']
-        dbhost = environment_dbhost.sub('-master', '') + '-replica'
-        run "mysqldump --add-drop-table -u #{dbuser} -h #{dbhost} -p #{environment_database} | bzip2 -c > #{backup_file}.bz2" do |ch, stream, out |
-           ch.send_data "#{dbpass}\n" if out=~ /^Enter password:/
-        end
-        run "bzcat #{backup_file}.bz2 | mysql -u #{dbuser} -p -h #{staging_dbhost} #{staging_database}" do |ch, stream, out|
+        run "gunzip < #{backup_file}.gz | mysql -u #{dbuser} -p -h #{staging_dbhost} #{staging_database}" do |ch, stream, out|
            ch.send_data "#{dbpass}\n" if out=~ /^Enter password:/
         end
       else
-        run "pg_dump -W -c -U #{dbuser} -h #{production_dbhost} #{production_database} | bzip2 -c > #{backup_file}.bz2" do |ch, stream, out|
-           ch.send_data "#{dbpass}\n" if out=~ /^Password:/
-        end
-        run "bzcat #{backup_file}.bz2 | psql -W -U #{dbuser} -h #{staging_dbhost} #{staging_database}" do |ch, stream, out|
+        run "gunzip < #{backup_file}.gz | psql -W -U #{dbuser} -h #{staging_dbhost} #{staging_database}" do |ch, stream, out|
            ch.send_data "#{dbpass}\n" if out=~ /^Password/
         end
       end
-      run "rm -f #{backup_file}.bz2"
+      run "rm -f #{backup_file}.gz"
     end
   
     desc "Backup your MySQL or PostgreSQL database to shared_path+/db_backups"
     task :dump, :roles => :db, :only => {:primary => true} do
-      backup_name
+      backup_name unless exists?(:backup_file)
+      on_rollback { run "rm -f #{backup_file}" }
       run("cat #{shared_path}/config/database.yml") { |channel, stream, data| @environment_info = YAML.load(data)[rails_env] }
+      
       if @environment_info['adapter'] == 'mysql'
         dbhost = @environment_info['host']
         dbhost = environment_dbhost.sub('-master', '') + '-replica' if dbhost != 'localhost' # added for Solo offering, which uses localhost
-        run "mysqldump --add-drop-table -u #{dbuser} -h #{dbhost} -p #{environment_database} | bzip2 -c > #{backup_file}.bz2" do |ch, stream, out |
+        run "mysqldump --add-drop-table -u #{dbuser} -h #{dbhost} -p #{environment_database} | gzip -c > #{backup_file}.gz" do |ch, stream, out |
            ch.send_data "#{dbpass}\n" if out=~ /^Enter password:/
         end
       else
-        run "pg_dump -W -c -U #{dbuser} -h #{environment_dbhost} #{environment_database} | bzip2 -c > #{backup_file}.bz2" do |ch, stream, out |
+        run "pg_dump -W -c -U #{dbuser} -h #{environment_dbhost} #{environment_database} | gzip -c > #{backup_file}.gz" do |ch, stream, out |
            ch.send_data "#{dbpass}\n" if out=~ /^Password:/
         end
       end
@@ -58,16 +52,17 @@ Capistrano::Configuration.instance(:must_exist).load do
     
     desc "Sync your production database to your local workstation"
     task :clone_to_local, :roles => :db, :only => {:primary => true} do
-      backup_name
+      backup_name unless exists?(:backup_file)
       dump
-      get "#{backup_file}.bz2", "/tmp/#{application}.sql.bz2"
+      get "#{backup_file}.gz", "/tmp/#{application}.sql.gz"
       development_info = YAML.load_file("config/database.yml")['development']
       if development_info['adapter'] == 'mysql'
-        run_str = "bzcat /tmp/#{application}.sql.bz2 | mysql -u #{development_info['username']} --password='#{development_info['password']}' -h #{development_info['host']} #{development_info['database']}"
+        run_str = "gunzip < /tmp/#{application}.sql.gz | mysql -u #{development_info['username']} --password='#{development_info['password']}' -h #{development_info['host']} #{development_info['database']}"
       else
-        run_str = "PGPASSWORD=#{development_info['password']} bzcat /tmp/#{application}.sql.bz2 | psql -U #{development_info['username']} -h #{development_info['host']} #{development_info['database']}"
+        run_str = "PGPASSWORD=#{development_info['password']} gunzip < /tmp/#{application}.sql.gz | psql -U #{development_info['username']} -h #{development_info['host']} #{development_info['database']}"
       end
       %x!#{run_str}!
+      run "rum -f #{backup_file}.gz"
     end
   end
 
