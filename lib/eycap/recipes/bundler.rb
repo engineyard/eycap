b113db402b3c15e09469c71c44b2827fadfbf06c
Matthew Dolian <mdignan@mdignan.local>
2011-01-28 16:57:37 -0500

    fixed bug report in ZD #20641

--- lib/eycap/recipes/bundler.rb
+++ lib/eycap/recipes/bundler.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :bundler do
     desc "Automatically installed your bundled gems if a Gemfile exists"
     task :bundle_gems do
-      run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && bundle install --without=test,development --path /var/cache/engineyard/; fi"
+      run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && bundle install --without=test development --path /var/cache/engineyard/; fi"
     end
     after "deploy:symlink_configs","bundler:bundle_gems"
   end
