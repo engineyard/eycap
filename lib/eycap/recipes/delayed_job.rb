44f038ea4c476865af33c11e524d5773baa7463c
Tyler Bird <tyler.bird@gmail.com>
2011-02-16 10:48:01 -0700

    best catch ever, change false to true

--- lib/eycap/recipes/delayed_job.rb
+++ lib/eycap/recipes/delayed_job.rb
@@ -3,21 +3,21 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc <<-DESC
     Start the Delayed Job queue along with any in the same monit_group.
     DESC
-    task :start, :roles => [:app], :only => {:dj => false} do
+    task :start, :roles => [:app], :only => {:dj => true} do
       sudo "/usr/bin/monit start all -g #{monit_group}"
     end
 
     desc <<-DESC
     Restart the Delayed Job queue along with any in the same monit_group.
     DESC
-    task :restart, :roles => [:app], :only => {:dj => false} do
+    task :restart, :roles => [:app], :only => {:dj => true} do
       sudo "/usr/bin/monit restart all -g #{monit_group}"
     end
 
     desc <<-DESC
     Stop all monit group members, of which delayed job can be a part of.
     DESC
-    task :stop, :roles => [:app], :only => {:dj => false} do
+    task :stop, :roles => [:app], :only => {:dj => true} do
       sudo "/usr/bin/monit stop all -g #{monit_group}"
     end
     
