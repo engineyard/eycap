129ec60d964ebbf9f79920bd047afb0b84caa63f
Matt Kern <mkern@artisantechnologies.com>
2011-02-16 16:09:32 -0800

    add --path to bundler call to avoid sudo and create the bundled_gems dir.

--- lib/eycap/recipes/bundler.rb
+++ lib/eycap/recipes/bundler.rb
@@ -1,8 +1,9 @@
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :bundler do
     desc "Automatically installed your bundled gems if a Gemfile exists"
-    task :bundle_gems do
-      run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && bundle install --without=test,development --binstubs #{release_path}/bin; fi"
+    task :bundle_gems 
+      run "mkdir -p #{shared_path}/bundled_gems"
+      run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && bundle install --without=test,development --binstubs #{release_path}/bin --path #{shared_path}/bundled_gems; fi"
     end
     after "deploy:symlink_configs","bundler:bundle_gems"
   end
