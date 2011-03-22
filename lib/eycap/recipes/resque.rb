d95df97699e7720554efba60588f850fb1fe5e30
Matt Dolian <dolian@MDmbPro.local>
2011-03-22 10:36:59 -0400

    added recipes for resque restart and symlink

---
+++ lib/eycap/recipes/resque.rb
@@ -0,0 +1,17 @@
+Capistrano::Configuration.instance(:must_exist).load do
+
+  namespace :resque do
+    desc "After deploy:restart we want to restart the workers"
+    task :restart, :roles => [:app], :only => {:resque => true} do
+      run "monit restart all -g resque_#{application}"
+    end
+    after "deploy:restart","resque:restart"
+    
+    desc "After update_code we want to symlink the resque.yml"
+    task :symlink, :roles => [:app], :only => {:resque => true} do
+      run "if [ -f #{shared_path}/config/resque.yml ]; then ln -nfs #{shared_path}/config/resque.yml #{latest_release}/config/resque.yml; fi"
+    end
+    after "deploy:update_code", "resque:symlink"
+  end 
+  
+end
