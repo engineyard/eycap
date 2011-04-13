490f238289f0213f8d2a24e6fcca3eed45b789ab
Tyler Bird <tyler.bird@gmail.com>
2011-04-13 11:35:45 -0600

    removed deploy hooks from resque recipe, which was making it fire for all users.

--- lib/eycap/recipes/resque.rb
+++ lib/eycap/recipes/resque.rb
@@ -5,13 +5,12 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :restart, :roles => [:app], :only => {:resque => true} do
       run "monit restart all -g resque_#{application}"
     end
-    after "deploy:restart","resque:restart"
     
     desc "After update_code we want to symlink the resque.yml"
     task :symlink, :roles => [:app], :only => {:resque => true} do
       run "if [ -f #{shared_path}/config/resque.yml ]; then ln -nfs #{shared_path}/config/resque.yml #{latest_release}/config/resque.yml; fi"
     end
-    after "deploy:symlink_configs", "resque:symlink"
+#    after "deploy:symlink_configs", "resque:symlink" add this to the deploy.rb file
   end 
   
 end
