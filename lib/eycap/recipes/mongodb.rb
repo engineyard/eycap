6480abf63ff633e60fcfafbe7ded1b4b79e2f366
Ines Sombra <isombra@engineyard.com>
2011-09-08 11:56:53 -0700

    adding sketchy code for mongo. Needs testing

--- lib/eycap/recipes/mongodb.rb
+++ lib/eycap/recipes/mongodb.rb
@@ -7,7 +7,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       now = Time.now
       run "mkdir -p #{shared_path}/db_backups"
       backup_time = [now.year,now.month,now.day,now.hour,now.min,now.sec].join('-')
-      set :backup_file, "#{shared_path}/db_backups/#{environment_database}-snapshot-#{backup_time}.sql"
+      set :backup_file, "#{shared_path}/db_backups/#{environment_database}-snapshot-#{backup_time}"
     end
 
     desc "Clone production database to staging database."
@@ -20,37 +20,21 @@ Capistrano::Configuration.instance(:must_exist).load do
       backup_name unless exists?(:backup_file)
       run("cat #{shared_path}/config/database.yml") { |channel, stream, data| @environment_info = YAML.load(data)[rails_env] }
       dump
-
-      if ['mysql', 'mysql2'].include? @environment_info['adapter']
-        run "gunzip < #{backup_file}.gz | mysql -u #{dbuser} -p -h #{staging_dbhost} #{staging_database}" do |ch, stream, out|
-           ch.send_data "#{dbpass}\n" if out=~ /^Enter password:/
-        end
-      else
-        run "gunzip < #{backup_file}.gz | psql -W -U #{dbuser} -h #{staging_dbhost} #{staging_database}" do |ch, stream, out|
-           ch.send_data "#{dbpass}\n" if out=~ /^Password/
-        end
-      end
+    
+      run "gunzip < #{backup_file}.gz | mongorestore -h #{staging_dbhost} #{staging_database} --drop "   # we assume target db is empty  do
+      if  "mongo --quiet --eval 'printjson(rs.isMaster()).ismaster' | grep '\"ismaster\" : true'"
+               
       run "rm -f #{backup_file}.gz"         
     end
 
-    desc "Backup your MySQL database to shared_path/db_backups."
-    task :dump, :roles => :db, :only => {:primary => true} do
+    desc "Backup your MongoDB database to shared_path/db_backups."
+    task :dump, :roles => :db, :only => {:primary => false} do
       backup_name unless exists?(:backup_file)
       on_rollback { run "rm -f #{backup_file}" }
       run("cat #{shared_path}/config/database.yml") { |channel, stream, data| @environment_info = YAML.load(data)[rails_env] }
       
-      if ['mysql', 'mysql2'].include? @environment_info['adapter']
-        dbhost = @environment_info['host']
-        if rails_env == "production"
-          dbhost = environment_dbhost.sub('-master', '') + '-replica' if dbhost != 'localhost' # added for Solo offering, which uses localhost
-        end
-        run "mysqldump --add-drop-table -u #{dbuser} -h #{dbhost} -p #{environment_database} | gzip -c > #{backup_file}.gz" do |ch, stream, out |
-           ch.send_data "#{dbpass}\n" if out=~ /^Enter password:/
-        end
-      else
-        run "pg_dump -W -c -U #{dbuser} -h #{environment_dbhost} #{environment_database} | gzip -c > #{backup_file}.gz" do |ch, stream, out |
-           ch.send_data "#{dbpass}\n" if out=~ /^Password:/
-        end
+      run "mongodump -d #{environment_database} | gzip -c > #{backup_file}.gz" do
+      if  "mongo --quiet --eval 'printjson(rs.isMaster()).ismaster' | grep '\"ismaster\" : false'"
       end
     end
 
@@ -61,15 +45,9 @@ Capistrano::Configuration.instance(:must_exist).load do
       get "#{backup_file}.gz", "/tmp/#{application}.sql.gz"
       development_info = YAML.load(ERB.new(File.read('config/database.yml')).result)['development']
 
-      if ['mysql', 'mysql2'].include? development_info['adapter']
-        run_str = "gunzip < /tmp/#{application}.sql.gz | mysql -u #{development_info['username']} --password='#{development_info['password']}' -h #{development_info['host']} #{development_info['database']}"
-      else
-        run_str  = ""
-        run_str += "PGPASSWORD=#{development_info['password']} " if development_info['password']
-        run_str += "gunzip < /tmp/#{application}.sql.gz | psql -U #{development_info['username']} "
-        run_str += "-h #{development_info['host']} "             if development_info['host']
-        run_str += development_info['database']
-      end
+      run_str = "gunzip < /tmp/#{application}.gz | mongorestore -h #{development_info['host']} -d #{development_info['database']} --drop" # we assume target db is empty do
+      if  "mongo --quiet --eval 'printjson(rs.isMaster()).ismaster' | grep '\"ismaster\" : true'"
+
       %x!#{run_str}!
       run "rm -f #{backup_file}.gz"
     end
