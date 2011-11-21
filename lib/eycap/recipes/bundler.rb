8f529efbba10f8328e4052417e631402d6744c17
Matt Dolian <matt.dolian@gmail.com>
2011-11-21 10:27:35 -0500

    symlink ey_bundler_binstubs to bin

--- lib/eycap/recipes/bundler.rb
+++ lib/eycap/recipes/bundler.rb
@@ -4,6 +4,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :bundle_gems do
       run "mkdir -p #{shared_path}/bundled_gems"
       run "if [ -f #{release_path}/Gemfile ]; then cd #{release_path} && bundle install --without=test development --binstubs #{release_path}/bin --path #{shared_path}/bundled_gems; fi"
+      run "ln -nfs #{release_path}/bin #{release_path}/ey_bundler_binstubs"  
     end
     after "deploy:symlink_configs","bundler:bundle_gems"
   end
