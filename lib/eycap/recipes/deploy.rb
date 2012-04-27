b83a7eaa2a6473e8982114654e10d65caa753512
Trevor Hart <trevor@limos.com>
2012-04-27 00:14:52 -0400

    Logging for additional deploy commands.

--- lib/eycap/recipes/deploy.rb
+++ lib/eycap/recipes/deploy.rb
@@ -3,7 +3,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   namespace :deploy do    
     # This is here to hook into the logger for deploy and deploy:long tasks
-    ["deploy", "deploy:long"].each do |tsk|
+    ["deploy", "deploy:long", "deploy:migrations", "deploy:migrate", "deploy:update_code"].each do |tsk|
       before(tsk) do
         Capistrano::EYLogger.setup( self, tsk )
         at_exit{ Capistrano::EYLogger.post_process if Capistrano::EYLogger.setup? }
