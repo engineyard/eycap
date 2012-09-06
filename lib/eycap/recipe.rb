82788cffa42744926ee68ad011f2cdbfa66f7d45
Tyler Bird <tyler.bird@gmail.com>
2012-09-06 16:09:23 -0600

    add doc for backward compatibility

--- lib/eycap/recipe.rb
+++ lib/eycap/recipe.rb
@@ -1,1 +1,4 @@
+# This is here to maintain backward compatibility for the <tt>require "eycap/recipe"</tt> 
+# statement at the beginning of each deploy.rb distributed before eycap 0.6.x
+
 require 'eycap'
