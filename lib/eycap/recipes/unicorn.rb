47161ac3e2a62083073f7e79b3d2da83da34c853
Tyler Bird <tyler.bird@gmail.com>
2012-09-21 14:33:52 -0600

    version bump

--- lib/eycap/recipes/unicorn.rb
+++ lib/eycap/recipes/unicorn.rb
@@ -27,21 +27,21 @@ Capistrano::Configuration.instance(:must_exist).load do
     Reloads the unicorn works gracefully - Use deploy task for deploys
     DESC
     task :reload, :roles => [:app], :except => {:unicorn => false} do
-      sudo "/engineyard/bin/unicorn #{application} reload"
+      run "/engineyard/bin/unicorn #{application} reload"
     end
 
     desc <<-DESC
     Adds a Unicorn worker - Beware of causing your host to swap, this setting isn't permanent
     DESC
     task :aworker, :roles => [:app], :except => {:unicorn => false} do
-      sudo "/engineyard/bin/unicorn #{application} aworker"
+      run "/engineyard/bin/unicorn #{application} aworker"
     end
 
     desc <<-DESC
     Removes a unicorn worker (gracefully)
     DESC
     task :rworker, :roles => [:app], :except => {:unicorn => false} do
-      sudo "/engineyard/bin/unicorn #{application} rworker"
+      run "/engineyard/bin/unicorn #{application} rworker"
     end
 
     desc <<-DESC
--- lib/eycap/version.rb
+++ lib/eycap/version.rb
@@ -1,4 +1,4 @@
 module Eycap
-  VERSION = '0.6.2'
+  VERSION = '0.6.3'
 end
 
