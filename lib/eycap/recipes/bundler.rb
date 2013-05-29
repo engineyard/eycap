4037942af28b052348e3f78f324f1b6e818eb16d
Tyler Bird <tyler.bird@gmail.com>
2013-05-28 23:06:04 -0600

    add bundle_config task

--- lib/eycap/recipes/bundler.rb
+++ lib/eycap/recipes/bundler.rb
@@ -17,7 +17,7 @@ set :bundle_without, "test development" unless exists?(:bundle_without)
         session.else <<-SHELL.split("\n").map(&:strip).join("; ")
           mkdir -p #{shared_path}/bundled_gems
           if [ -f #{release_path}/Gemfile ]
-          then cd #{release_path} && bundle install --without=#{bundle_without} --binstubs #{release_path}/bin --path #{shared_path}/bundled_gems
+          then cd #{release_path} && bundle install --without=#{bundle_without} --binstubs #{release_path}/bin --path #{shared_path}/bundled_gems --quiet
           fi
           if [ ! -h #{release_path}/bin ]
           then ln -nfs #{release_path}/bin #{release_path}/ey_bundler_binstubs
@@ -25,6 +25,15 @@ set :bundle_without, "test development" unless exists?(:bundle_without)
         SHELL
       end
     end
+    task :bundle_config, :roles => :app, :only => {:no_bundle => true} do
+      <<-SHELL.split("\n").map(&:strip).join("; ")
+        bundle config --local BIN /data/#{application}/releases/#{release_path}/bin
+        bundle config --local PATH /data/#{application}/shared/bundled_gems
+        bundle config --local DISABLE_SHARED_GEMS "1"
+        bundle config --local WITHOUT test:development 
+      SHELL
+    end
     after "deploy:symlink_configs","bundler:bundle_gems"
+    after "bundler:bundle_gems","bundler:bundle_config"
   end
 end
